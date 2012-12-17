7265f8815666c2c38c29e3afc6f3841d5e9895dd
Ivan Sim <ihcsim@gmail.com>
2012-12-16 17:08:10 -0800

    Add test to check for valid, non-null states drop-down after invoking geoselector.

--- test/test-cases/geoselector-test.js
+++ test/test-cases/geoselector-test.js
@@ -1,4 +1,9 @@
-test("Create GeoSelector country drop-down", function() {
+test("Create GeoSelector - Valid country drop-down", function() {
   $("#geoselector-form").geoSelector();
   ok($("#country"), "Country drop-down should not be null");
+});
+
+test("Create GeoSelector - Valid state drop-down", function() {
+  $("#geoselector-form").geoSelector();
+  ok($("#state"), "Country drop-down should not be null");
 });
