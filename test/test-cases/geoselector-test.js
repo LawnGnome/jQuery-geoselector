4c023c1c080e6e584faaef2d49f8be7df315d0fa
Ivan Sim <ihcsim@gmail.com>
2012-12-16 17:50:49 -0800

    Change some unit tests titles.

--- test/test-cases/geoselector-test.js
+++ test/test-cases/geoselector-test.js
@@ -1,9 +1,19 @@
-test("Create GeoSelector - Valid country drop-down", function() {
+test("Create GeoSelector - Valid country drop-down list", function() {
   $("#geoselector-form").geoSelector();
-  ok($("*[name='country']"), "Country drop-down should not be null");
+  ok($("*[name='country']"), "Country drop-down list should not be null");
 });
 
-test("Create GeoSelector - Valid state drop-down", function() {
+test("Create GeoSelector - Valid state drop-down list", function() {
   $("#geoselector-form").geoSelector();
-  ok($("*[name='state']"), "Country drop-down should not be null");
+  ok($("*[name='state']"), "Country drop-down list should not be null");
+});
+
+test("Create GeoSelector - Country drop-down list type", function() {
+  $("#geoselector-form").geoSelector();
+  ok($("*[name='country']")[0].type.toLowerCase() == "select-one", "Country drop-down list type should be 'select-one'");
+});
+
+test("Create GeoSelector - State drop-down list type", function() {
+  $("#geoselector-form").geoSelector();
+  ok($("*[name='state']")[0].type.toLowerCase() == "select-one", "State drop-down list type should be 'select-one'");
 });
