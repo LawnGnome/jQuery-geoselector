23ee10548cf90c4cc34c44ca67b27b8a94b0e17a
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-17 11:17:40 -0800

    Add more unit tests to test preservation of HTML attributes.

--- test/test-cases/preserve-attributes.js
+++ test/test-cases/preserve-attributes.js
@@ -21,3 +21,35 @@ test("Preserve Attributes - Country drop-down list 'class' attribute", function(
 test("Preserve Attributes - State drop-down list 'class' attribute", function() {
   equal($("*[name='state']").attr("class"), "dropdown", "State drop-down list 'class' attribute should be preserved");
 });
+
+test("Preserve Attributes - Country drop-down list 'size' attribute", function() {
+  equal($("*[name='country']").attr("size"), "1", "Country drop-down list 'size' attribute should be preserved");
+});
+
+test("Preserve Attributes - State drop-down list 'size' attribute", function() {
+  equal($("*[name='state']").attr("size"), "1", "State drop-down list 'size' attribute should be preserved");
+});
+
+test("Preserve Attributes - Country drop-down list 'form' attribute", function() {
+  equal($("*[name='country']").attr("form"), "geoselector-form", "Country drop-down list 'form' attribute should be preserved");
+});
+
+test("Preserve Attributes - State drop-down list 'form' attribute", function() {
+  equal($("*[name='state']").attr("form"), "geoselector-form", "State drop-down list 'form' attribute should be preserved");
+});
+
+test("Preserve Attributes - Country drop-down list 'disabled' attribute", function() {
+  equal($("*[name='country']").attr("disabled"), "disabled", "Country drop-down list 'disabled' attribute should be preserved");
+});
+
+test("Preserve Attributes - State drop-down list 'disabled' attribute", function() {
+  equal($("*[name='state']").attr("disabled"), "disabled", "State drop-down list 'disabled' attribute should be preserved");
+});
+
+test("Preserve Attributes - Country drop-down list 'autofocus' attribute", function() {
+  equal($("*[name='country']").attr("autofocus"), "autofocus", "Country drop-down list 'autofocus' attribute should be preserved");
+});
+
+test("Preserve Attributes - State drop-down list 'autofocus' attribute", function() {
+  equal($("*[name='state']").attr("autofocus"), "autofocus", "State drop-down list 'autofocus' attribute should be preserved");
+});
