0e345d8e3218fe2368d923b9249fec531119f980
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-17 11:53:34 -0800

    Update tear down codes to re-select default country and state after each test is completed.

--- test/test-cases/teardown.js
+++ test/test-cases/teardown.js
@@ -2,4 +2,7 @@ QUnit.done(removeSelected);
 function removeSelected(){
   $("*[name='country']").find("option[selected='selected']").removeAttr("selected");
   $("*[name='state']").find("option[selected='selected']").removeAttr("selected");
+  
+  $("*[name='country']").find("option[code='AU']").attr("selected", "selected").trigger("change");
+  $("*[name='state']").find("option[code='Western Australia']").attr("selected", "selected");
 }
