d04e14e1dc4664f4ae583d6853b87c0a3f63b84f
Ivan Sim <ihcsim@gmail.com>
2012-12-16 17:56:06 -0800

    Promote geoselector function call in unit test to global level.

--- test/test-cases/create-obj.js
+++ test/test-cases/create-obj.js
@@ -1,19 +1,16 @@
+$("#geoselector-form").geoSelector();
 test("Create GeoSelector - Valid country drop-down list", function() {
-  $("#geoselector-form").geoSelector();
   ok($("*[name='country']"), "Country drop-down list should not be null");
 });
 
 test("Create GeoSelector - Valid state drop-down list", function() {
-  $("#geoselector-form").geoSelector();
   ok($("*[name='state']"), "Country drop-down list should not be null");
 });
 
 test("Create GeoSelector - Country drop-down list type", function() {
-  $("#geoselector-form").geoSelector();
   ok($("*[name='country']")[0].type.toLowerCase() == "select-one", "Country drop-down list type should be 'select-one'");
 });
 
 test("Create GeoSelector - State drop-down list type", function() {
-  $("#geoselector-form").geoSelector();
   ok($("*[name='state']")[0].type.toLowerCase() == "select-one", "State drop-down list type should be 'select-one'");
 });
