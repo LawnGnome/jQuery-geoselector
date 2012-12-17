fd013a3db8060a1011fa203f60ec82b41f405c90
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-17 10:21:07 -0800

    Replace 'ok' assertions with 'equal' assertions.

--- test/test-cases/preserve-attributes.js
+++ test/test-cases/preserve-attributes.js
@@ -1,7 +1,7 @@
 test("Preserve Attributes - Country drop-down list 'name' attribute", function() {
-  ok($("*[name='country']"), "Country drop-down list 'name' attribute should be preserved");
+  equal($("*[name='country']").attr("name"), "country", "Country drop-down list 'name' attribute should be preserved");
 });
 
 test("Preserve Attributes - State drop-down list 'name' attribute", function() {
-  ok($("*[name='state']"), "State drop-down list 'name' attribute should be preserved");
+  equal($("*[name='state']").attr("name"), "state", "State drop-down list 'name' attribute should be preserved");
 });
