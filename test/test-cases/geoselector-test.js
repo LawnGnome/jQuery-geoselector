8b46f20640bbb879e3fdce0091bad61f2b54fdcb
Ivan Sim <ihcsim@gmail.com>
2012-12-16 17:39:46 -0800

    Replace selectors in unit tests to use the 'name' attribute as per geoselector internal implementation.

--- test/test-cases/geoselector-test.js
+++ test/test-cases/geoselector-test.js
@@ -1,9 +1,9 @@
 test("Create GeoSelector - Valid country drop-down", function() {
   $("#geoselector-form").geoSelector();
-  ok($("#country"), "Country drop-down should not be null");
+  ok($("*[name='country']"), "Country drop-down should not be null");
 });
 
 test("Create GeoSelector - Valid state drop-down", function() {
   $("#geoselector-form").geoSelector();
-  ok($("#state"), "Country drop-down should not be null");
+  ok($("*[name='state']"), "Country drop-down should not be null");
 });
